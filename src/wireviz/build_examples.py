a9e733747cace8dc1e48fab677304ce4606b65c8
KV <kvid@users.noreply.github.com>
2020-07-22 17:27:02 +0200

    Rename some variables to better reflect their contents and relations

--- src/wireviz/build_examples.py
+++ src/wireviz/build_examples.py
@@ -14,16 +14,16 @@ from wv_helper import open_file_write, open_file_read, open_file_append
 
 
 readme = 'readme.md'
-paths = {}
-paths['examples'] = {'path': Path(script_path).parent.parent.parent / 'examples',
+groups = {}
+groups['examples'] = {'path': Path(script_path).parent.parent.parent / 'examples',
                      'prefix': 'ex',
                      readme: [], # Include no files
                      'title': 'Example Gallery'}
-paths['tutorial'] = {'path': Path(script_path).parent.parent.parent / 'tutorial',
+groups['tutorial'] = {'path': Path(script_path).parent.parent.parent / 'tutorial',
                      'prefix': 'tutorial',
                      readme: ['md', 'yml'], # Include .md and .yml files
                      'title': 'WireViz Tutorial'}
-paths['demos']    = {'path': Path(script_path).parent.parent.parent / 'examples',
+groups['demos']    = {'path': Path(script_path).parent.parent.parent / 'examples',
                      'prefix': 'demo'}
 
 input_extensions = ['.yml']
@@ -31,26 +31,26 @@ generated_extensions = ['.gv', '.png', '.svg', '.html', '.bom.tsv']
 extensions_not_from_graphviz = [ext for ext in generated_extensions if ext[-1] == 'v']
 
 
-def collect_filenames(description, pathkey, ext_list, extrafile = None):
-    path = paths[pathkey]['path']
-    patterns = [f"{paths[pathkey]['prefix']}*{ext}" for ext in ext_list]
+def collect_filenames(description, groupkey, ext_list, extrafile = None):
+    path = groups[groupkey]['path']
+    patterns = [f"{groups[groupkey]['prefix']}*{ext}" for ext in ext_list]
     if extrafile is not None:
         patterns.append(extrafile)
     print(f"{description} {path}")
     return sorted([filename for pattern in patterns for filename in path.glob(pattern)])
 
 
-def build(dirname):
+def build_generated(groupkey):
     # build files
-    path = paths[dirname]['path']
-    build_readme = readme in paths[dirname]
+    path = groups[groupkey]['path']
+    build_readme = readme in groups[groupkey]
     if build_readme:
-        include_readme = 'md' in paths[dirname][readme]
-        include_source = 'yml' in paths[dirname][readme]
+        include_readme = 'md' in groups[groupkey][readme]
+        include_source = 'yml' in groups[groupkey][readme]
         with open_file_write(path / readme) as out:
-            out.write(f'# {paths[dirname]["title"]}\n\n')
+            out.write(f'# {groups[groupkey]["title"]}\n\n')
     # collect and iterate input YAML files
-    for yaml_file in collect_filenames('Building', dirname, input_extensions):
+    for yaml_file in collect_filenames('Building', groupkey, input_extensions):
         print(f'  {yaml_file}')
         wireviz.parse_file(yaml_file)
 
@@ -78,33 +78,33 @@ def build(dirname):
                 out.write(f'[Source]({yaml_file.name}) - [Bill of Materials]({yaml_file.stem}.bom.tsv)\n\n\n')
 
 
-def clean_generated(pathkeys):
-    for key in pathkeys:
+def clean_generated(groupkeys):
+    for key in groupkeys:
         # collect and remove files
-        for filename in collect_filenames('Cleaning', key, generated_extensions, readme if readme in paths[key] else None):
+        for filename in collect_filenames('Cleaning', key, generated_extensions, readme if readme in groups[key] else None):
             if filename.is_file():
                 print(f'  rm {filename}')
                 os.remove(filename)
 
 
-def compare_generated(pathkeys, include_from_graphviz = False):
+def compare_generated(groupkeys, include_from_graphviz = False):
     compare_extensions = generated_extensions if include_from_graphviz else extensions_not_from_graphviz
-    for key in pathkeys:
+    for key in groupkeys:
         # collect and compare files
-        for filename in collect_filenames('Comparing', key, compare_extensions, readme if readme in paths[key] else None):
+        for filename in collect_filenames('Comparing', key, compare_extensions, readme if readme in groups[key] else None):
             cmd = f'git --no-pager diff {filename}'
             print(f'  {cmd}')
             os.system(cmd)
 
 
-def restore_generated(pathkeys):
-    for key in pathkeys:
+def restore_generated(groupkeys):
+    for key in groupkeys:
         # collect input YAML files
         filename_list = collect_filenames('Restoring', key, input_extensions)
         # collect files to restore
         filename_list = [fn.with_suffix(ext) for fn in filename_list for ext in generated_extensions]
-        if readme in paths[key]:
-            filename_list.append(paths[key]['path'] / readme)
+        if readme in groups[key]:
+            filename_list.append(groups[key]['path'] / readme)
         # restore files
         for filename in filename_list:
             cmd = f'git checkout -- {filename}'
@@ -115,15 +115,15 @@ def restore_generated(pathkeys):
 def parse_args():
     parser = argparse.ArgumentParser(description='Wireviz Example Manager',)
     parser.add_argument('action', nargs='?', action='store', choices=['build','clean','compare','restore'], default='build')
-    parser.add_argument('-generate', nargs='*', choices=paths.keys(), default=paths.keys())
+    parser.add_argument('-generate', nargs='*', choices=groups.keys(), default=groups.keys())
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
     if args.action == 'build':
-        for gentype in args.generate:
-            build(gentype)
+        for groupkey in args.generate:
+            build_generated(groupkey)
     elif args.action == 'clean':
         clean_generated(args.generate)
     elif args.action == 'compare':
